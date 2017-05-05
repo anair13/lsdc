34fe78249255d82c3b1983917bfa261f8d8b8a91
Ashvin Nair <nair.ashvin@gmail.com>
2017-05-05 14:08:22 -0700

    experiment

---
+++ python/video_prediction/inverse/experiments/hard/forward_32_temp_0.2.py
@@ -0,0 +1,22 @@
+import sys
+import os
+sys.path.insert(1, os.path.join(sys.path[0], '../..'))
+
+import transforming_dynamics_model
+import collections
+
+import tensorflow as tf
+
+if __name__ == "__main__":
+    new_conf = transforming_dynamics_model.get_conf(
+        experiment_name="mining",
+        fsize=32,
+        mu2=1e-6,
+        mu3 = 1,
+        autoencoder="decode",
+        loadalldata=500,
+        miningtemp=2 * (10.0 ** (-i)),
+    )
+
+    model = transforming_dynamics_model.DynamicsModel(new_conf)
+    model.train(50000)
